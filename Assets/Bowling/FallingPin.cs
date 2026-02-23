f5ff250ba20bfd379693b89e57faf39e7bc81abb
2021642337 <2021642337@ENS-12-01>
2026-02-23 14:14:37 +0100

    script fallingpin

---
+++ Assets/Bowling/FallingPin.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPin : MonoBehaviour
+{
+
+    float fallAngleThreshold = 45f;
+
+    bool isFallen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.SetActive(!isFallen);
+        if (!isFallen)
+        {
+            float angle = Vector3.Angle(transform.up, Vector3.up);
+
+            if (angle < fallAngleThreshold)
+            {
+                isFallen = true;
+                Debug.Log(gameObject.name + " est tombee !");
+            }
+        }
+    }
+}
